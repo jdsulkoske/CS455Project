6840d628f7583ee2af0e05e5236b675b6ee45a36
frshatat <farisshatat4@yahoo.com>
2016-02-10 12:22:24 -0500

    Inputted number values in matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -11,51 +11,33 @@ public class Matrix {
 
     private ArrayList<String> csvDataArray = new ArrayList<String>();
     private ArrayList newArray = new ArrayList();
-    private String[] splitData;
-    private String input;
-    private int index;
-    private double average;
+
     Map<String, Integer> map = new HashMap<String, Integer>();
     private ArrayList<String> keyValueArray;
     String[][] dataArr;
 
     public static void main(String[] args) {
         Matrix data = new Matrix();
-        data.parseData("Case beat", 0);
-        //data.printGraph(RandomArray());
+        data.parseData();
+
         System.out.println();
 
 
     }
 
-    public void parseData(String input, int index) {
+    public void parseData() {
         CSVReader bufferReader = null;
         csvDataArray.clear();
         newArray.clear();
 
-        this.input = input;
-        this.index = index;
 
         try {
-            String[] line;
+
             bufferReader = new CSVReader(new FileReader("src/CSV2.csv"));
-
-
-            //while ((line = bufferReader.readNext()) != null) {
-                List<String[]> list = bufferReader.readAll();
-
-// Convert to 2D array
-                 dataArr = new String[list.size()][];
-                dataArr = list.toArray(dataArr);
-                printGraph(dataArr);
-           // System.out.print(dataArr);
-                //for(int i= 0;i<line.length;i++) {
-                       // splitCSVToArray(line[i]);
-
-
-               // }
-                //    findFrequency();
-          //  }
+            List<String[]> list = bufferReader.readAll();
+            dataArr = new String[list.size()][];
+            dataArr = list.toArray(dataArr);
+            printGraph(getMatrix());
 
 
         } catch (IOException e) {
@@ -67,32 +49,33 @@ public class Matrix {
                 e.printStackTrace();
             }
         }
-        //while (i < 1001) {
-
-       // }
 
     }
-    public ArrayList<String> splitCSVToArray(String csv) {
-        if (csv != null) {
-            splitData = csv.split("\\s*,\\s*");
-            for (int i = 0; i < splitData.length; i++) {
-                if (!(splitData[i] == null) || !(splitData[i].length() == 0)) {
-                    csvDataArray.add(splitData[i].trim());
-                }
-            }
-        }
 
-        return csvDataArray;
-    }
 
-    public void findFrequency() {
+    public void assignNumberValueToKey() {
         keyValueArray = new ArrayList<String>();
         int count = 0;
-        for (int i = 1; i < csvDataArray.size(); i++) {
-            if (map.containsKey(csvDataArray.get(i))) {
-                map.put(csvDataArray.get(i), map.get(csvDataArray.get(i)));
-            } else {
-                map.put(csvDataArray.get(i), count++);
+        for (int i = 0; i < dataArr.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (map.containsKey(dataArr[i][j])) {
+                    if(j==3){
+                        map.put(dataArr[i][j], Integer.valueOf(dataArr[i][j]));
+                    }
+                    if( dataArr[i][j].equals("TRUE")){
+                        map.put(dataArr[i][j], 1);
+
+                    }
+                    if( dataArr[i][j].equals("FALSE")){
+                        map.put(dataArr[i][j], 0);
+
+                    }
+                    else {
+                        map.put(dataArr[i][j], map.get(dataArr[i][j]));
+                    }
+                } else {
+                    map.put(dataArr[i][j], count++);
+                }
             }
 
         }
@@ -100,17 +83,14 @@ public class Matrix {
             keyValueArray.add((String) k);
         }
 
-        map.clear();
-        keyValueArray.clear();
     }
 
-    public String[][] getMatrix(){
-        String[][] randomMatrix = new String [1001][4];
-        for (int i = 1; i < 1001; i++) {
-            for(int j=0;j<1;j++) {
-                //System.out.println(i + ". " + map.get(csvDataArray.get(i)));
-
-                    randomMatrix[i][j] = csvDataArray.get(i);
+    public int[][] getMatrix() {
+        assignNumberValueToKey();
+        int[][] randomMatrix = new int[1000][4];
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 0; j < 4; j++) {
+                randomMatrix[i][j] = map.get(dataArr[i][j]);
 
             }
         }
@@ -120,7 +100,7 @@ public class Matrix {
 
 
     public static int[][] RandomArray() {
-        int[][] randomMatrix = new int [1000][4];
+        int[][] randomMatrix = new int[1000][4];
 
         Random rand = new Random();
         //rand.setSeed(System.currentTimeMillis());
@@ -134,23 +114,22 @@ public class Matrix {
 
         return randomMatrix;
     }
-    int i = 0;
-    public void printGraph(String[][] array) {
-        int l = 0;
-        //while (l<4) {
-        for(int i=0;i<1000;i++) {
+
+
+
+    public void printGraph(int[][] array) {
+        for (int i = 1; i < 1000; i++) {
             for (int j = 0; j < 4; j++) {
-                if(array[i][j]!=null) {
-                    System.out.print(array[i][j] + "|");
-                }
+                System.out.print(array[i][j] + "| ");
+
+
 
             }
             System.out.println();
-            //l++;
-        }  /// / i++;
 
         }
 
+    }
 
 
 }
