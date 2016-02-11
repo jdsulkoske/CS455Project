2efac9f3f67c84bcd01346c0efdb6a301dfe4781
frshatat <farisshatat4@yahoo.com>
2016-02-11 15:10:59 -0500

    Normalized the 4th column and converted categorical attribute to ordinal

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,7 +1,10 @@
 import au.com.bytecode.opencsv.CSVReader;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.*;
 
 /**
@@ -11,15 +14,12 @@ public class Matrix {
 
     private ArrayList<String> csvDataArray = new ArrayList<String>();
     private ArrayList newArray = new ArrayList();
-
-    Map<String, Integer> map = new HashMap<String, Integer>();
+    Map<String, Double> map = new HashMap<String, Double>();
     private ArrayList<String> keyValueArray;
     String[][] dataArr;
-
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         Matrix data = new Matrix();
         data.parseData();
-
         System.out.println();
 
 
@@ -32,12 +32,12 @@ public class Matrix {
 
 
         try {
-
-            bufferReader = new CSVReader(new FileReader("src/CSV2.csv"));
+            bufferReader = new CSVReader(new FileReader("src/Crimes_-_2016.csv"));
             List<String[]> list = bufferReader.readAll();
             dataArr = new String[list.size()][];
             dataArr = list.toArray(dataArr);
-            printGraph(getMatrix());
+            printCSV1(randomMatrix());
+            printCSV2(getMatrix());
 
 
         } catch (IOException e) {
@@ -55,43 +55,124 @@ public class Matrix {
 
     public void assignNumberValueToKey() {
         keyValueArray = new ArrayList<String>();
-        int count = 0;
+        double count = 0;
+        int min = getMin();
+        int max = getMax();
         for (int i = 0; i < dataArr.length; i++) {
             for (int j = 0; j < 4; j++) {
                 if (map.containsKey(dataArr[i][j])) {
-                    if(j==3){
-                        map.put(dataArr[i][j], Integer.valueOf(dataArr[i][j]));
+                    if (j == 3) {
+                        double s = (Integer.valueOf(dataArr[i][j]) - min);
+                        double normalize = (Double.valueOf(dataArr[i][j]) - min) / (max - min);
+                        DecimalFormat df = new DecimalFormat(("#.00"));
+                        normalize = Double.parseDouble(df.format(normalize));
+                        map.put(dataArr[i][j], normalize);
                     }
-                    if( dataArr[i][j].equals("TRUE")){
-                        map.put(dataArr[i][j], 1);
+                    if (dataArr[i][j].equals("TRUE")) {
+                        map.put(dataArr[i][j], 1.0);
 
                     }
-                    if( dataArr[i][j].equals("FALSE")){
-                        map.put(dataArr[i][j], 0);
+                    if (dataArr[i][j].equals("FALSE")) {
+                        map.put(dataArr[i][j], 0.0);
 
-                    }
-                    else {
-                        map.put(dataArr[i][j], map.get(dataArr[i][j]));
+                    } else {
+                        checkAndConvertCategoricalAttributeToNumberValue(i, j);
                     }
                 } else {
                     map.put(dataArr[i][j], count++);
+
                 }
             }
 
+
         }
+
         for (Object k : map.keySet()) {
             keyValueArray.add((String) k);
         }
 
     }
 
-    public int[][] getMatrix() {
+    public int getMax() {
+        int max = 0;
+        for (int i = 1; i < dataArr.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                int number = Integer.parseInt(dataArr[i][3]);
+                if (number > max) {
+                    max = number;
+                }
+            }
+        }
+        return max;
+    }
+
+    public int getMin() {
+        int min = Integer.parseInt(dataArr[1][3]);
+        for (int i = 1; i < dataArr.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                int number = Integer.parseInt(dataArr[i][3]);
+                if (number < min) {
+                    min = number;
+                }
+            }
+        }
+        return min;
+
+
+    }
+
+    private void checkAndConvertCategoricalAttributeToNumberValue(int i, int j) {
+        if (j == 0 && (dataArr[i][j].equals("LIQUOR LAW VIOLATION")
+                || dataArr[i][j].equals("OTHER OFFENSE") ||
+                dataArr[i][j].equals("NON-CRIMINAL (SUBJECT SPECIFIED)") ||
+                dataArr[i][j].equals("STALKING ")
+                ||
+                dataArr[i][j].equals("PUBLIC PEACE VIOLATION"))) {
+            map.put(dataArr[i][j], 1.0);
+        }
+        if (j == 0 && (dataArr[i][j].equals("THEFT")
+                || dataArr[i][j].equals("BURGLARY") ||
+                dataArr[i][j].equals("CRIMINAL TRESPASS") ||
+                dataArr[i][j].equals("DECEPTIVE PRACTICE")
+                ||
+                dataArr[i][j].equals("INTIMIDATION"))) {
+
+            map.put(dataArr[i][j], 2.0);
+        }
+        if (j == 0 && (dataArr[i][j].equals("NARCOTICS")
+                || dataArr[i][j].equals("CRIMINAL DAMAGE") ||
+                dataArr[i][j].equals("INTERFERENCE WITH PUBLIC OFFICER") ||
+                dataArr[i][j].equals("WEAPONS VIOLATION")
+                ||
+                dataArr[i][j].equals("BATTERY"))) {
+            map.put(dataArr[i][j], 3.0);
+        }
+        if (j == 0 && (dataArr[i][j].equals("ARSON")
+                || dataArr[i][j].equals("PROSTITUTION") ||
+                dataArr[i][j].equals("ROBBERY") ||
+                dataArr[i][j].equals("MOTOR VEHICLE THEFT")
+                ||
+                dataArr[i][j].equals("CRIM SEXUAL ASSAULT"))) {
+            map.put(dataArr[i][j], 4.0);
+        }
+        if (j == 0 && (dataArr[i][j].equals("ASSAULT")
+                || dataArr[i][j].equals("OFFENSE INVOLVING CHILDREN") ||
+                dataArr[i][j].equals("SEX OFFENSE") ||
+                dataArr[i][j].equals("KIDNAPPING")
+                ||
+                dataArr[i][j].equals("HOMICIDE"))) {
+            map.put(dataArr[i][j], 5.0);
+        } else {
+            map.put(dataArr[i][j], map.get(dataArr[i][j]));
+        }
+    }
+
+    public double[][] getMatrix() {
         assignNumberValueToKey();
-        int[][] randomMatrix = new int[1000][4];
+        double[][] randomMatrix = new double[1000][4];
         for (int i = 1; i < 1000; i++) {
             for (int j = 0; j < 4; j++) {
                 randomMatrix[i][j] = map.get(dataArr[i][j]);
-
             }
         }
 
@@ -99,15 +180,13 @@ public class Matrix {
     }
 
 
-    public static int[][] RandomArray() {
-        int[][] randomMatrix = new int[1000][4];
-
+    public String[][] randomMatrix() {
+        String[][] randomMatrix = new String[1000][4];
         Random rand = new Random();
-        //rand.setSeed(System.currentTimeMillis());
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 4; j++) {
-                Integer r = rand.nextInt(10000);
-                randomMatrix[i][j] = r;
+                Integer r = rand.nextInt(dataArr.length);
+                randomMatrix[i][j] = dataArr[r][j];
             }
 
         }
@@ -116,20 +195,44 @@ public class Matrix {
     }
 
 
-
-    public void printGraph(int[][] array) {
+    public void printCSV2(double[][] array) throws IOException {
+        BufferedWriter br = new BufferedWriter(new FileWriter("CSV2.csv"));
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i < 1000; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.print(array[i][j] + "| ");
-
-
+                System.out.print(array[i][j] + ", ");
+                sb.append(array[i][j]);
+                sb.append(",");
 
             }
+            sb.append("\n");
             System.out.println();
 
         }
+        br.write(sb.toString());
+        br.close();
 
     }
 
+    public void printCSV1(String[][] array) throws IOException {
+
+        BufferedWriter br = new BufferedWriter(new FileWriter("CSV1.csv"));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(array[i][j] + ", ");
+                sb.append(array[i][j]);
+                sb.append(",");
+
+            }
+            sb.append("\n");
+            System.out.println();
+
+        }
+        br.write(sb.toString());
+        br.close();
+
+    }
 
 }
