ddbbc4438cb759f6d9486a1bee3a79b5c314fd8a
frshatat <farisshatat4@yahoo.com>
2016-04-03 19:40:40 -0400

    Information Gain function

--- src/Project4.java
+++ src/Project4.java
@@ -3,11 +3,7 @@ import au.com.bytecode.opencsv.CSVReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by FarisShatat on 3/25/16.
@@ -21,6 +17,7 @@ public class Project4 {
     private BufferedWriter br;
     private StringBuilder sb;
     Map<String, Integer> map = new HashMap<String, Integer>();
+    Map<String, Integer> countMap = new HashMap<String, Integer>();
     private ArrayList<String> keyValueArray;
 
     public static void main(String[] args) throws IOException {
@@ -53,10 +50,10 @@ public class Project4 {
     }
 
     private String[][] getCSV2Matrix() {
-        String[][] matrix = new String[100][4];
+        String[][] matrix = new String[100][5];
         for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 4; j++) {
-                    matrix[i][j] = dataArr[i][j];
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = dataArr[i][j];
 
             }
         }
@@ -89,45 +86,81 @@ public class Project4 {
 
     public void findFrequency() {
         keyValueArray = new ArrayList<String>();
+        ArrayList<String> countValueArray = new ArrayList<String>();
+        ArrayList<Double> resultArray = new ArrayList<>();
 
-        for (int i = 1; i < dataItems.length; i++) {
+        for(int l =0;l<5;l++) {
+            for (int i = 1; i < dataItems.length; i++) {
+                if (l != 2) {
+                    if (dataItems[i][2].equals("TRUE")) {
+                        if (map.containsKey(dataItems[i][l] + " is True")) {
+                            map.put(dataItems[i][l] + " is True", map.get(dataItems[i][l] + " is True") + 1);
 
-                 if (dataItems[i][2].equals("TRUE")) {
-                    if (map.containsKey(dataItems[i][0] + " is True")) {
-                        map.put(dataItems[i][0] + " is True", map.get(dataItems[i][0]+ " is True") + 1);
+                        } else {
+                            map.put(dataItems[i][l] + " is True", 1);
+
+                        }
+                    } else if (dataItems[i][2].equals("FALSE")) {
+                        if (map.containsKey(dataItems[i][l] + " is False")) {
+                            map.put(dataItems[i][l] + " is False", map.get(dataItems[i][l] + " is False") + 1);
+
+                        } else {
+                            map.put(dataItems[i][l] + " is False", 1);
+
+
+                        }
                     }
-                     else{
-                        map.put(dataItems[i][0] + " is True", 1);
-                    }
-                 }
-                 else if (dataItems[i][2].equals("FALSE")) {
-                     if (map.containsKey(dataItems[i][0] + " is False")) {
-                         map.put(dataItems[i][0] + " is False",map.get(dataItems[i][0]+ " is False") + 1);
-                     }
-                     else{
-                         map.put(dataItems[i][0] + " is False", 1);
-
-                     }
-                 }
+                }
 
 
+            }
+            for (Object k : map.keySet()) {
+                keyValueArray.add((String) k);
+                Collections.sort(keyValueArray);
+            }
+            double results = 0;
+            double size = 0;
+            for (int j = 0; j < keyValueArray.size(); j += 2) {
+                if (j != keyValueArray.size() - 1) {
+                    double value = map.get(keyValueArray.get(j));
+                    double a = map.get(keyValueArray.get(j + 1));
+                    size = value + a;
+
+                    double calc = -(Math.log(value / size) / Math.log(2) * (value / size));
+                    double t = -(Math.log(a / size) / Math.log(2)) * (a / size);
+                    double r = calc + t;
+                    //System.out.println(size);
+                    double csize = size / dataItems.length;
+                    //System.out.println(csize);
+                    double p = csize * r;
+                    //System.out.println(p);
+                    results = +p;
+
+
+                }
+
+                // System.out.println(a);
+            }
+            System.out.println(results);
+            System.out.println(map.size());
+//            resultArray.add(results);
+//            for (int i = 0)
+//                System.out.println("Entropy 1 " + results);
+
+
+//        DecimalFormat df = new DecimalFormat(("#.00"));
+//        for (int i = 0; i < map.size(); i++) {
+//            double value = (map.get(keyValueArray.get(i)));
+//            value = (value / dataArr.length) * 100;
+//            String newValue = df.format(value);
+//
+//            //System.out.println(keyValueArray.get(i) + " is " + newValue + "%" +"; frequency: "+ map.get(keyValueArray.get(i)));
+//        }
+            System.out.println("______________________________________________");
+
+            map.clear();
+            keyValueArray.clear();
         }
-        for (Object k : map.keySet()) {
-            keyValueArray.add((String) k);
-        }
-
-        DecimalFormat df = new DecimalFormat(("#.00"));
-        for (int i = 0; i < map.size(); i++) {
-            double value = (map.get(keyValueArray.get(i)));
-            value = (value / dataArr.length) * 100;
-            String newValue = df.format(value);
-
-            System.out.println(keyValueArray.get(i) + " is " + newValue + "%" +"; frequency: "+ map.get(keyValueArray.get(i)));
-        }
-        System.out.println("______________________________________________");
-
-        map.clear();
-        keyValueArray.clear();
     }
 
 //    private String[][] newMatrix() {
